36c0fff6b7f17e6aaafc080399ec713c0e69ab46
Shagun_Singh <shagun.singh2112@gmail.com>
2026-04-27 06:02:00 +0530

    Update Folio

---
+++ src/sections/Work.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WorkPage from '../components/work/WorkPage';
+
+const Work: React.FC = () => {
+  return (
+    <section id="work">
+      <WorkPage />
+    </section>
+  );
+};
+
+export default Work;
